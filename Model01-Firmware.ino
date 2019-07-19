e2539df08f0a726542e729a7c6b60fc4efa71518
Judson <nyarly@gmail.com>
2019-07-19 13:03:31 -0700

    Resolving several little issues

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -142,7 +142,7 @@ enum { MACRO_VERSION_INFO,
   *
   */
 
-enum { PRIMARY, NUMPAD, FUNCTION }; // layers
+enum { PRIMARY, FUNCTION, NUMPAD }; // layers
 
 
 /**
@@ -231,19 +231,19 @@ KEYMAPS(
 #elif defined (PRIMARY_KEYMAP_CUSTOM)
   // Edit this keymap to make a custom layout
   [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Escape,
-   Key_Tab,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_LeftGui, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Enter,
-   Key_LeftControl, Key_Spacebar, Key_LeftGui, Key_LeftShift,
+  (___,                     Key_1,         Key_2,        Key_3,          Key_4,  Key_5,  Key_Escape,
+   Key_Backtick,            Key_Q,         Key_W,        Key_E,          Key_R,  Key_T,  Key_Tab,
+   Key_Backslash,           Key_A,         Key_S,        Key_D,          Key_F,  Key_G,
+   Key_LeftGui,             Key_Z,         Key_X,        Key_C,          Key_V,  Key_B,  Key_Enter,
+   Key_LeftControl,         Key_Spacebar,  Key_LeftGui,  Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Escape,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Backspace, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
+     M(MACRO_ANY),             Key_6,        Key_7,          Key_8,             Key_9,       Key_0,          LockLayer(NUMPAD),
+     Key_Escape,               Key_Y,        Key_U,          Key_I,             Key_O,       Key_P,          Key_Equals,
+     /*dead ,                  */Key_H,      Key_J,          Key_K,             Key_L,       Key_Semicolon,  Key_Quote,
+     Key_RightAlt,             Key_N,        Key_M,          Key_Comma,         Key_Period,  Key_Slash,      Key_Minus,
+     Key_RightShift,           Key_LeftAlt,  Key_Backspace,  Key_RightControl,
+     ShiftToLayer(FUNCTION)),
 
 #else
 
@@ -260,12 +260,21 @@ KEYMAPS(
 #define Key_DoubleQuote LSHIFT(Key_Quote)
 #define Key_LeftAngleBracket LSHIFT(Key_Comma)
 #define Key_RightAngleBracket LSHIFT(Key_Period)
-  /*
-  Ergodox typing:
-  _+{}"
-  -=()'
-  <>[]|
-  */
+
+  [FUNCTION] =  KEYMAP_STACKED
+  (___,  Key_F1,                Key_F2,                 Key_F3,                Key_F4,                 Key_F5,           Key_LEDEffectNext,
+   ___,  Key_Underscore,        Key_Plus,               Key_LeftCurlyBracket,  Key_RightCurlyBracket,  Key_DoubleQuote,  ___,
+   ___,  Key_Minus,             Key_Equals,             Key_LeftParen,         Key_RightParen,         Key_Quote,
+   ___,  Key_LeftAngleBracket,  Key_RightAngleBracket,  Key_LeftBracket,       Key_RightBracket,       Key_Pipe,         ___,
+   ___,  ___,                   ___,                    ShiftToLayer(NUMPAD),
+   ___,
+
+     Consumer_ScanPreviousTrack,  Key_F6,           Key_F7,                    Key_F8,                    Key_F9,          Key_F10,           Key_F11,
+     Consumer_PlaySlashPause,     Key_Home,         Key_PageDown,              Key_PageUp,                Key_End,         Key_RightBracket,  Key_F12,
+     /*dead ,                     */Key_LeftArrow,  Key_DownArrow,             Key_UpArrow,               Key_RightArrow,  ___,               ___,
+     Consumer_ScanNextTrack,      Consumer_Mute,    Consumer_VolumeDecrement,  Consumer_VolumeIncrement,  ___,             Key_Backslash,     Key_Pipe,
+     ShiftToLayer(NUMPAD),        ___,              Key_Delete,                ___,
+     ___),
 
   [NUMPAD] =  KEYMAP_STACKED
   (___,       ___,              ___,          ___,            ___,            ___,               ___,
@@ -275,30 +284,29 @@ KEYMAPS(
    ___,       Key_Delete,       ___,          ___,
    ___,
 
-   M(MACRO_VERSION_INFO),  ___, Key_7, Key_8,      Key_9,              Key_KeypadSubtract, ___,
-   ___,                    ___, Key_4, Key_5,      Key_6,              Key_KeypadAdd,      ___,
-                           ___, Key_1, Key_2,      Key_3,              Key_Equals,         ___,
-   ___,                    ___, Key_0, Key_Period, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
-   ___, ___, ___, ___,
-   ___),
+     M(MACRO_VERSION_INFO),  ___,    Key_7,  Key_8,       Key_9,               Key_KeypadSubtract,  ___,
+     ___,                    ___,    Key_4,  Key_5,       Key_6,               Key_KeypadAdd,       ___,
+     /*dead ,                */___,  Key_1,  Key_2,       Key_3,               Key_Equals,          ___,
+     ___,                    ___,    Key_0,  Key_Period,  Key_KeypadMultiply,  Key_KeypadDivide,    Key_Enter,
+     ___,                    ___,    ___,    ___,
+     ___)
 
+#else
   [FUNCTION] =  KEYMAP_STACKED
   (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
-   ___, Key_Underscore, Key_Plus, Key_LeftCurlyBracket, Key_RightCurlyBracket, Key_DoubleQuote, ___,
-   ___, Key_Minus, Key_Equals, Key_LeftParen, Key_RightParen, Key_Quote,
-   ___, Key_LeftAngleBracket, Key_RightAngleBracket, Key_LeftBracket, Key_RightBracket, Key_Pipe, ___,
-
-   ___, Key_Enter, ___, ShiftToLayer(NUMPAD),
+   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
+   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
+   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+   ___, Key_Enter, ___, ___,
    ___,
 
+   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
+   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
+                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
+   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   ShiftToLayer(NUMPAD), ___, Key_Delete, ___,
+   ___),
 
-   Consumer_ScanPreviousTrack,  Key_F6,         Key_F7,                    Key_F8,                    Key_F9,   Key_F10,           Key_F11,
-   Consumer_PlaySlashPause,     Key_Home,       Key_PageDown,              Key_PageUp,                Key_End,  Key_RightBracket,  Key_F12,
-                                Key_LeftArrow,  Key_DownArrow,             Key_UpArrow,               Key_RightArrow,            ___,      ___,
-   Consumer_ScanNextTrack,      Consumer_Mute,  Consumer_VolumeDecrement,  Consumer_VolumeIncrement,  ___,      Key_Backslash,     Key_Pipe,
-   ___,                                        ___,            Key_Enter,                 ___,
-   ___)
-#else
   [NUMPAD] =  KEYMAP_STACKED
   (___, ___, ___, ___, ___, ___, ___,
    ___, ___, ___, ___, ___, ___, ___,
@@ -312,21 +320,6 @@ KEYMAPS(
                            ___, Key_1, Key_2,      Key_3,              Key_Equals,         ___,
    ___,                    ___, Key_0, Key_Period, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
    ___, ___, ___, ___,
-   ___),
-
-  [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   ___, Key_Delete, ___, ___,
-   ___,
-
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ShiftToLayer(NUMPAD), ___, Key_Delete, ___,
    ___)
 #endif
 ) // KEYMAPS(

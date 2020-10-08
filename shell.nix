908d2e3d29f1397dfb448623d5000a2f40823f96
Judson Lester <nyarly@gmail.com>
2020-10-08 15:28:13 -0700

    Updating firmware, shell.nix fixes

--- shell.nix
+++ shell.nix
@@ -2,10 +2,10 @@ let
   unstable = import ./unstable.nix;
 in
 { pkgs ? unstable }:
-pkgs.mkShell {
+removeAttrs (pkgs.mkShell {
   buildInputs = with pkgs; [
     arduino
   ];
 
   ARDUINO_PATH = "${pkgs.arduino}/share/arduino";
-}
+}) [ "CXX" "CC" "AR" ] # otherwise they override arduino tools
